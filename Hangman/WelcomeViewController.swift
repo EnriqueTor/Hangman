c6bdb8f6fd33ff4786a07a3d048898ca22811d91
EnriqueTor <etorrendell@gmail.com>
2017-02-15 23:06:43 -0500

    added fonts and views

--- Hangman/WelcomeViewController.swift
+++ Hangman/WelcomeViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  WelcomeViewController.swift
 //  Hangman
 //
 //  Created by Enrique Torrendell on 2/15/17.
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class WelcomeViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
