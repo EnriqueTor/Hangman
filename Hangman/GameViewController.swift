da577b41df6973024ce15e0cf8314940a5e8d949
EnriqueTor <etorrendell@gmail.com>
2017-02-16 14:45:14 -0500

    added keyboard functionality

--- Hangman/GameViewController.swift
+++ Hangman/GameViewController.swift
@@ -9,31 +9,63 @@
 import UIKit
 
 class GameViewController: UIViewController {
-
+    
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var livesLabel: UILabel!
+    @IBOutlet weak var secretWordLabel: UILabel!
+    
+    
+    
+    @IBOutlet var keyboardButtons: [UIButton]!
+    
     let store = HangmanData.sharedInstance
-
     
-    
-    
+    var userName = "Enrique"
+    var lives = 6
+    var secretWord = ""
+    var wordCharacter = 0
+    var hiddenWord = ""
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-
+        
+        
+        nameLabel.text = userName
+        livesLabel.text = "\(lives)"
+        
         store.getWordFromAPI { (currentData) in
             
             print("WE ARE IN WELCOME VC")
             print(currentData)
             print("YEAH")
             
+            self.secretWord = currentData
+            
+            for _ in self.secretWord.characters {
+                
+                self.hiddenWord.append("_ ")
+                
+            }
+            
+            
+            
+            
+            DispatchQueue.main.async {
+                
+                self.secretWordLabel.text = self.hiddenWord
+            }
         }
-
         
-    
+        
     }
-
-   
+    
+    
+    @IBAction func letterPressed(_ sender: UIButton) {
+        
+        print(sender.titleLabel?.text)
+        
+    }
     
     
     
