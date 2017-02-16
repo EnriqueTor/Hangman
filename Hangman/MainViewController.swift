4c8201ba16d50ecec1142f95f1ab41e4c8481130
EnriqueTor <etorrendell@gmail.com>
2017-02-15 23:54:04 -0500

    added cocoapods

---
+++ Hangman/MainViewController.swift
@@ -0,0 +1,45 @@
+//
+//  MainViewController.swift
+//  Hangman
+//
+//  Created by Enrique Torrendell on 2/15/17.
+//  Copyright © 2017 Tor. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+  
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+}
---
+++ Podfile
@@ -0,0 +1,20 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'Hangman' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for Hangman
+
+  pod 'Firebase'
+  pod 'Firebase/Core'
+  pod 'Firebase/Auth'
+  pod 'Firebase/Database'
+  pod 'Firebase/Storage'
+  pod 'Firebase/Invites'
+  pod 'SDWebImage', '~>3.8'
+  pod 'SwiftKeychainWrapper'
+  pod 'IQKeyboardManagerSwift'
+
+end
