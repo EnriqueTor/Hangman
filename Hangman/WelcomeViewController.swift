7001338e2f15a9f291cb91a7a2e90c2cf05e062d
EnriqueTor <etorrendell@gmail.com>
2017-02-16 20:04:10 -0500

    added keychain wrapper files and created the bridge

--- Hangman/WelcomeViewController.swift
+++ Hangman/WelcomeViewController.swift
@@ -7,10 +7,13 @@
 //
 
 import UIKit
+import Firebase
 
 class WelcomeViewController: UIViewController {
     
     let store = HangmanData.sharedInstance
+    let myKeychainWrapper = KeychainWrapper()
+    let database = FIRDatabase.database().reference()
     
     override func viewDidLoad() {
         super.viewDidLoad()        
@@ -36,6 +39,50 @@ class WelcomeViewController: UIViewController {
 
     }
     
+    func welcome() {
+        
+        func loginUser() {
+            
+            if UserDefaults.standard.value(forKey: "email") as? String == nil {
+                
+                return
+                
+            }
+                
+            else {
+                
+                let email = UserDefaults.standard.value(forKey: "email") as? String
+                let pass = myKeychainWrapper.myObject(forKey: "v_Data") as? String
+                
+                FIRAuth.auth()?.signIn(withEmail: email!, password: pass!) { (user, error) in
+                    
+                    if error != nil {
+                        
+                    }
+                        
+                    else {
+                        
+                        let userData = self.database.child("users").child((user?.uid)!)
+                        
+                        // update daily list
+                        
+                        userData.observe(.value, with: { (snapshot) in
+                            
+                            let data = snapshot.value as? [String:Any]
+                            let loggedUser = User(id: "", username: "", email: "", profilePic: "")
+                            self.store.user = loggedUser.deserialize(data!)
+                            
+                            NotificationCenter.default.post(name: Notification.Name.openMainVC, object: nil)
+                            
+                        })
+                    }
+                }
+            }
+        }
+
+        
+        
+    }
     
 }
 
